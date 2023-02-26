2f3d529c234a5cd6b9a3e9165e269849747f6a2d
Harshitrai01 <90034825+Harshitrai01@users.noreply.github.com>
2023-02-26 10:21:51 +0530

    Updated Solution - GfG to GitHub

--- Easy/Number of Unique Paths/number-of-unique-paths.cpp
+++ Easy/Number of Unique Paths/number-of-unique-paths.cpp
@@ -10,16 +10,32 @@ class Solution
 {
     public:
     //Function to find total number of unique paths.
+    // int solve(int a, int b, int i, int j){
+    //     if(i=0 or j=0)
+    //         return 0;
+    //     if(i==1 and j==1)
+    //         return 1;
+    //     int left=solve(a,b,i,j-1);
+    //     int up=solve(a,b,i-1,j);
+    //     return right+down;
+    // }
     int NumberOfPath(int a, int b)
-    {
-        vector<vector<int>>dp(a,vector<int>(b,1));
-        dp[0][0]=1;
-        for(int i=1;i<a;i++){
-            for(int j=1;j<b;j++){
-                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+    {   
+        vector<vector<int>>dp(a+1,vector<int>(b+1,0));
+        for(int i=0;i<=a;i++){
+            for(int j=0;j<=b;j++){
+                if(i==1 and j==1)
+                    dp[i][j]=1;
+                else if(i==0 or j==0)
+                    dp[i][j]=0;
+                else{
+                    int left=dp[i][j-1];
+                    int up=dp[i-1][j];
+                    dp[i][j]=left+up;                    
+                }
             }
         }
-        return dp[a-1][b-1];
+        return dp[a][b];
     }
 };
 
