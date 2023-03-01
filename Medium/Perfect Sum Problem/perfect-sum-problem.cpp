550faf0950958be2d2cd479f59a14f9e8b4aa7ab
Harshitrai01 <90034825+Harshitrai01@users.noreply.github.com>
2023-03-01 10:31:33 +0530

    Updated Solution - GfG to GitHub

--- Medium/Perfect Sum Problem/perfect-sum-problem.cpp
+++ Medium/Perfect Sum Problem/perfect-sum-problem.cpp
@@ -7,22 +7,35 @@ class Solution{
 
 	public:
 	int mod=1e9+7;
-	int CountSubsetSum(int arr[], int n, int sum, vector<vector<int>> &dp){
-	    if(dp[n][sum]!=-1) return dp[n][sum];
-	    if(n==0){
-	        if(sum==0) return 1;
-	        else return 0;
-	    }
-	    else if(arr[n-1]>sum)
-	        return dp[n][sum]=CountSubsetSum(arr,n-1,sum,dp)%mod;
-	    else
-	        return dp[n][sum]=(CountSubsetSum(arr,n-1,sum-arr[n-1],dp)+CountSubsetSum(arr,n-1,sum,dp))%mod;
+// 	int CountSubsetSum(int arr[], int n, int sum, vector<vector<int>> &dp){
+// 	    if(dp[n][sum]!=-1) return dp[n][sum];
+// 	    if(n==0){
+// 	        if(sum==0) return 1;
+// 	        else return 0;
+// 	    }
+// 	    else if(arr[n-1]>sum)
+// 	        return dp[n][sum]=CountSubsetSum(arr,n-1,sum,dp)%mod;
+// 	    else
+// 	        return dp[n][sum]=(CountSubsetSum(arr,n-1,sum-arr[n-1],dp)+CountSubsetSum(arr,n-1,sum,dp))%mod;
 	    
-	}
+// 	}
 	
 	int perfectSum(int arr[], int n, int sum)
-	{      vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
-           return CountSubsetSum(arr,n,sum,dp)%mod;
+	{      vector<vector<int>>dp(n+1,vector<int>(sum+1));
+	       for(int i=0;i<=n;i++){
+	           for(int j=0;j<=sum;j++){
+	               if(i==0){
+	                   if(j==0) dp[i][j]=1;
+	                   else dp[i][j]=0;
+	               }
+	               else if(arr[i-1]>j){
+	                   dp[i][j]=dp[i-1][j]%mod;
+	               }
+	               else
+	                dp[i][j]=(dp[i-1][j]+dp[i-1][j-arr[i-1]])%mod;
+	           }
+	       }
+           return dp[n][sum]%mod;
 	}
 	  
 };
