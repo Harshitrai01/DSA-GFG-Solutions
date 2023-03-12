3235c866e6ce29de94b18773c1c7dac0dd918b38
Harshitrai01 <90034825+Harshitrai01@users.noreply.github.com>
2023-03-12 17:58:59 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Missing number/missing-number.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int missingNumber(int a[], int n);
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int i=0, n;
+		cin>>n;
+		int a[n+5];
+		for(i=0;i<n-1;i++)
+			cin>>a[i];
+			
+		cout<<missingNumber(a, n)<<endl;
+	}
+}
+// } Driver Code Ends
+
+
+int missingNumber(int A[], int n)
+{   int sum1=n*(n+1)/2;
+    int sum2=0;
+   for(int i=0;i<n-1;i++){
+       sum2+=A[i];
+   }
+   return sum1-sum2;
+}
