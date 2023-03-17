5898c5ff56589b3d36374d901eb417356a2e8644
Harshitrai01 <90034825+Harshitrai01@users.noreply.github.com>
2023-03-17 19:35:10 +0530

    Updated Solution - GfG to GitHub

--- Medium/Buy Stock 2/buy-stock-2.cpp
+++ Medium/Buy Stock 2/buy-stock-2.cpp
@@ -5,31 +5,14 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
     public:
-    long long maximumProfit(vector<long long>&arr, int n) {
-        arr.push_back(-1);
-        vector<vector<long long>>ans;
-        vector<long long>temp;
-        
-        bool flag=true;
-        for(int i=1;i<n+1;i++){
-            if(arr[i-1]<=arr[i]){
-                if(flag)
-                temp.push_back(i-1);
-                flag=false;
+    long long maximumProfit(vector<long long>&prices, int n) {
+        long long profit=0;
+        for(int i=1;i<n;i++){
+            if(prices[i-1]<prices[i]){
+                profit+=prices[i]-prices[i-1];
             }
-            else{
-                temp.push_back(i-1);
-                if(temp.size()==2)
-                ans.push_back(temp);
-                temp.clear();
-                flag=true;
-            }
-        }
-        long long res=0;
-        for(int i=0;i<ans.size();i++){
-            res+=(arr[ans[i][1]]-arr[ans[i][0]]);
-        }
-        return res;
+        } 
+        return profit;
     }
 };
 
