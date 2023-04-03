c546379c8364d16512de241a0341f97780756629
Harshitrai01 <90034825+Harshitrai01@users.noreply.github.com>
2023-04-03 20:17:49 +0530

    Added Solution - GfG to GitHub

---
+++ School/Replace all 0's with 5/replace-all-0s-with-5.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+ 
+// Driver program to test above function
+
+// } Driver Code Ends
+class Solution{
+  public:
+    /*you are required to complete this method*/
+    int convertFive(int n)
+    {
+        string s=to_string(n);
+        for(auto &x:s){
+            if(x=='0')
+                x='5';
+        }
+        return stoi(s);
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+    	int n;
+    	cin>>n;
+    	Solution obj;
+    	cout<<obj.convertFive(n)<<endl;
+    }
+}
+// } Driver Code Ends
