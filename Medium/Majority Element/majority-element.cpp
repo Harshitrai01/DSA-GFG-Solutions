2ed88ed7c3d77f7e11257b1255429b2e40968901
Harshitrai01 <90034825+Harshitrai01@users.noreply.github.com>
2023-04-09 23:45:54 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Majority Element/majority-element.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+     // Function to find majority element in the array
+    // a: input array
+    // size: size of input array
+int majorityElement(int arr[], int size)
+{
+        
+    int res=0,count=1;
+    for(int i=1;i<size;i++){
+        
+        if(arr[res]==arr[i])
+            count++;
+            
+        else
+            count--;
+            
+        if(count==0){
+            res=i;count=1;
+        }
+      
+    }
+    count=0;
+    for(int i=0;i<size;i++){
+        if(arr[res]==arr[i])
+        count++;
+        }
+            if(count<=size/2)
+            return -1;
+            
+            return arr[res];
+            
+}
+};
+
+//{ Driver Code Starts.
+
+int main(){
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+        int arr[n];
+        
+        for(int i = 0;i<n;i++){
+            cin >> arr[i];
+        }
+        Solution obj;
+        cout << obj.majorityElement(arr, n) << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
