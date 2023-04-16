18e6ddfd4e80bcb396b45ef79919a5d5200a5064
Harshit Kumar Rai <90034825+Harshitrai01@users.noreply.github.com>
2023-04-17 01:55:12 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Number of Provinces/number-of-provinces.cpp
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+  
+    void bfs(vector<int> list[], vector<bool>&vis, int i){
+        queue<int>q;
+        q.push(i);
+        vis[i]=true;
+        while(!q.empty()){
+            int u=q.front();
+            q.pop();
+            for(auto x:list[u]){
+                if(vis[x]==false){
+                    vis[x]=true;
+                    q.push(x);
+                }
+            }
+        }
+    }
+  
+    int numProvinces(vector<vector<int>> adj, int V){
+        vector<int> list[V];
+        for(int i=0;i<V;i++){
+            for(int j=0;j<V;j++){
+                if(i!=j and adj[i][j]==1){
+                    list[i].push_back(j);
+                    list[j].push_back(i);                    
+                }
+            }
+        }
+        int ans=0;
+        vector<bool>vis(V,false);
+        for(int i=0;i<V;i++){
+            if(vis[i]==false){
+                ans++;
+                bfs(list,vis,i);
+            }
+        }
+        return ans;
+    }
+    
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int V,x;
+        cin>>V;
+        
+        vector<vector<int>> adj;
+        
+        for(int i=0; i<V; i++)
+        {
+            vector<int> temp;
+            for(int j=0; j<V; j++)
+            {
+                cin>>x;
+                temp.push_back(x);
+            }
+            adj.push_back(temp);
+        }
+        
+
+        Solution ob;
+        cout << ob.numProvinces(adj,V) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
