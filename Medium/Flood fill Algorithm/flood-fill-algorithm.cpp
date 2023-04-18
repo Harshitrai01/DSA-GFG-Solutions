fc75be233cff8b7db7b403a91f348bec68354862
Harshit Kumar Rai <90034825+Harshitrai01@users.noreply.github.com>
2023-04-18 13:33:38 +0530

    Updated Solution - GfG to GitHub

--- Medium/Flood fill Algorithm/flood-fill-algorithm.cpp
+++ Medium/Flood fill Algorithm/flood-fill-algorithm.cpp
@@ -5,28 +5,24 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
 public:
-    int d_row[4]={-1,0,1,0};
-    int d_col[4]={0,1,0,-1};
-    void solve(vector<vector<int>>& image,int i,int j,int color,int m,int n,int initial){
-        if(i<0 or j<0 or i>=m or j>=n or image[i][j]==color)
-            return;
-        if(image[i][j]==initial){
-            image[i][j]=color;
-            
-            for(int k=0;k<4;k++){
-                int n_row=i+d_row[k];
-                int n_col=j+d_col[k];
-                solve(image,n_row,n_col,color,m,n,initial);
-            }
+
+    void dfs(vector<vector<int>>&image, int sr, int sc, int source, int color){
+        int n=image.size(),m=image[0].size();
+        int drow[]={0,0,1,-1};
+        int dcol[]={-1,1,0,0};
+        image[sr][sc]=color;
+        for(int k=0;k<=3;k++){
+            int i=sr+drow[k];
+            int j=sc+dcol[k];
+            if(i>=0 and i<n and j>=0 and j<m and image[i][j]==source)
+                dfs(image,i,j,source,color);
         }
-        
     }
 
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int m=image.size();
-        int n=image[0].size();
-        int initial=image[sr][sc];
-        solve(image,sr,sc,color,m,n,initial);
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        int source=image[sr][sc];
+        if(source==newColor) return image;
+        dfs(image,sr,sc,source,newColor);
         return image;
     }
 };
