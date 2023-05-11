10e77f08533a504dc1bb4a7d0055af3f7fad0c8e
Harshit Kumar Rai <90034825+Harshitrai01@users.noreply.github.com>
2023-05-11 22:55:56 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Implement strstr/implement-strstr.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+int strstr(string ,string);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string a;
+        string b;
+        
+        cin>>a;
+        cin>>b;
+        
+        cout<<strstr(a,b)<<endl;
+    }
+}
+
+// } Driver Code Ends
+
+
+//Function to locate the occurrence of the string x in the string s.
+int strstr(string s, string x)
+{
+     size_t found = s.find(x);
+        if (found != string::npos)
+            return found;
+        return -1;
+}
