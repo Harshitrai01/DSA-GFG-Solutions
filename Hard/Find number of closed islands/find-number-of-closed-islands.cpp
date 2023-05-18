383d8776dab8457f59c339f97a35a3e88c02fb5e
Harshit Kumar Rai <90034825+Harshitrai01@users.noreply.github.com>
2023-05-18 11:13:42 +0530

    Updated Solution - GfG to GitHub

--- Hard/Find number of closed islands/find-number-of-closed-islands.cpp
+++ Hard/Find number of closed islands/find-number-of-closed-islands.cpp
@@ -9,42 +9,58 @@ using namespace std;
 
 class Solution {
     public:
-    void dfs(vector<vector<int>>& grid, vector<vector<int>>&vis, int i, int j){
-        int n=grid.size();
-        int m=grid[0].size();
-        vis[i][j]=1;
-        int drow[]={0,0,-1,1};
-        int dcol[]={-1,1,0,0};
-        for(int k=0;k<=3;k++){
-            int r=i+drow[k];
-            int c=j+dcol[k];
-            if(r>=0 and r<n and c>=0 and c<m and !vis[r][c] and grid[i][j]==1){
-                vis[r][c]=1;
-                dfs(grid,vis,r,c);
-            }
-        }
-    }
-
-    int closedIslands(vector<vector<int>>& grid, int n, int m) {
+    int closedIslands(vector<vector<int>>& matrix, int n, int m) {
         vector<vector<int>>vis(n,vector<int>(m,0));
+        queue<pair<int,int>>q;
+        
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if((i==0 or i==n-1 or j==0 or j==m-1) and !vis[i][j] and grid[i][j]==1){
-                    dfs(grid,vis,i,j);
+                if((i==0 or i==n-1 or j==0 or j==m-1) and matrix[i][j]==1){
+                    q.push({i,j});
+                    vis[i][j]=1;
+                }
+            }
+        }
+        int drow[]={0,0,1,-1};
+        int dcol[]={1,-1,0,0};
+        
+        while(!q.empty()){
+            int i=q.front().first;
+            int j=q.front().second;
+            q.pop();
+            for(int k=0;k<=3;k++){
+                int nr=drow[k]+i;
+                int nc=dcol[k]+j;
+                if(nr>=0 and nr<n and nc>=0 and nc<m and !vis[nr][nc] and matrix[nr][nc]==1){
+                    vis[nr][nc]=1;
+                    q.push({nr,nc});
                 }
             }
         }
         int ans=0;
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++)
-                if(!vis[i][j] and grid[i][j]==1){
-                    dfs(grid,vis,i,j);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!vis[i][j] and matrix[i][j]==1){
                     ans++;
+                    q.push({i,j});
+                    while(!q.empty()){
+                        int r=q.front().first;
+                        int c=q.front().second;
+                        q.pop();
+                        for(int k=0;k<=3;k++){
+                            int nr=drow[k]+r;
+                            int nc=dcol[k]+c;
+                            if(nr>=0 and nr<n and nc>=0 and nc<m and !vis[nr][nc] and matrix[nr][nc]==1){
+                                vis[nr][nc]=1;
+                                q.push({nr,nc});
+                            }
+                        }
+                    }
                 }
-
+            }
+        }
         return ans;
     }
-
 };
 
 //{ Driver Code Starts.
