fb1b52d7820e10ba30cf21b6ea87b382e4a2eba4
Harshit Kumar Rai <90034825+Harshitrai01@users.noreply.github.com>
2023-06-14 16:35:07 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Maximum Diamonds/maximum-diamonds.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    long long maxDiamonds(int A[], int N, int K) {
+        long long ans=0;
+        priority_queue<int>q;
+        for(int i=0;i<N;i++){
+            q.push(A[i]);
+        }
+        while(K--){
+            int u=q.top();
+            ans+=u;
+            q.pop();
+            q.push(u/2);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N,K;
+        
+        cin>>N>>K;
+        int A[N];
+        
+        for(int i=0; i<N; i++)
+            cin>>A[i];
+
+        Solution ob;
+        cout << ob.maxDiamonds(A,N,K) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
